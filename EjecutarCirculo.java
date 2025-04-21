adc588e0bdaa50d4e4b8e8e3df4133928a77a044
200619c <mcvillada@estudiante.uniajc.edu.co>
2025-04-21 20:11:06 +0000

    Clase circulo

---
+++ EjecutarCirculo.java
@@ -0,0 +1,11 @@
+public class EjecutarCirculo {
+    public static void main(String[] args){
+    
+        // creacion del odjeto
+        Circulo objCirculo1 = new Circulo(r);
+
+        System.out.println("perimetro: " + objCirculo1.calcularPerimetro());
+
+        System.out.println("area: "+ objCirc )
+    }
+}
