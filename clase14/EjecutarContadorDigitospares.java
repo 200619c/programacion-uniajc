b1a35c7b415d293c24b2b5388c8d747394bb5e0a
200619c <mcvillada@estudiante.uniajc.edu.co>
2025-05-12 20:17:24 +0000

    creacion del punto1 del parcial

---
+++ clase14/EjecutarContadorDigitospares.java
@@ -0,0 +1,8 @@
+public class EjecutarContadorDigitoPares {
+    public static void main(String[] args) {
+        int numero = 6792;
+        NumerosPares objNum1 = new NumerosPares();
+        System.out.println("La cantidad de números pares es: " + objNum1.contarPares(numero));
+    }
+
+}
